53546ec03798ce3c5eb3a986400a0bf2802037b9
Surya Dhanush <dhanush.surya01@gmail.com>
2020-08-11 01:20:11 +0530

    Complete: Ask master password toggle functionality

--- app/src/main/java/com/cyberviy/ViyP/SplashActivity.java
+++ app/src/main/java/com/cyberviy/ViyP/SplashActivity.java
@@ -3,17 +3,23 @@ package com.cyberviy.ViyP;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 public class SplashActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 3000;
+    String PREF_NAME = "Settings";
+    String PREF_KEY = "MASTER_PASSWORD";
+    SharedPreferences sharedPreferences;
+
 
     // Gradient on statusbar
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -35,9 +41,19 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, MLock.class));
-                finish();
+                sharedPreferences = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+                final boolean askPasswordLaunchState = sharedPreferences.getBoolean(PREF_KEY, true);
+                if (!askPasswordLaunchState) {
+                    startActivity(new Intent(SplashActivity.this, Home.class));
+                    Toast.makeText(getApplicationContext(), "Consider using Master lock for maximum privacy", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    startActivity(new Intent(SplashActivity.this, MLock.class));
+                    finish();
+                }
             }
         }, SPLASH_TIME_OUT);
+
+
     }
 }
